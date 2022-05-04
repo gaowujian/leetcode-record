6f54d3699c4522848ad259d7aa1f612e2d0da8d0
tony <806003097@qq.com>
2022-05-05 07:55:27 +0800

    update

--- 206.反转链表.js
+++ 206.反转链表.js
@@ -16,31 +16,51 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-function ListNode(val, next) {
-  this.val = val === undefined ? 0 : val;
-  this.next = next === undefined ? null : next;
-}
+// function ListNode(val, next) {
+//   this.val = val === undefined ? 0 : val;
+//   this.next = next === undefined ? null : next;
+// }
+
+// head表示需要反转链表的头节点
 var reverseList = function (head) {
   if (!head) return null;
-  // const dummyNode = new ListNode(null, head);
-  // let prev = dummyNode;
-  let prev = null;
-  while (head) {
-    // 记录前一个元素
-    // let tmp = prev;
-    // prev = head;
-    // head = head.next;
-    // prev.next = tmp;
-    // 记录后一个元素
-    let tmp = head.next;
-    head.next = prev;
-    prev = head;
-    head = tmp;
+  let slow = null;
+  let fast = head;
+  while (fast) {
+    let nextFast = fast.next;
+    fast.next = slow;
+    slow = fast;
+    fast = nextFast;
   }
-  return prev;
+  return slow;
 };
 // @lc code=end
 
 // @after-stub-for-debug-begin
 module.exports = reverseList;
 // @after-stub-for-debug-end
+
+// * 迭代写法
+// var reverseList = function (head) {
+//   if (!head) return null;
+
+//   let prev = null;
+//   while (head) {
+//     let tmp = head.next;
+//     head.next = prev;
+//     prev = head;
+//     head = tmp;
+//   }
+//   return prev;
+// };
+
+// * 递归写法
+// var reverseList = function (head) {
+//   if (!head) return null;
+//   if (!head.next) return head;
+//   const reversedHead = reverseList(head.next);
+
+//   head.next.next = head;
+//   head.next = null;
+//   return reversedHead;
+// };
--- 53.最大子数组和.js
+++ 53.最大子数组和.js
@@ -9,8 +9,40 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxSubArray = function(nums) {
-
+var maxSubArray = function (nums) {
+  if (!nums) return;
+  if (nums.length === 1) {
+    return nums[0];
+  }
+  let max = -Infinity;
+  for (let i = 0; i < nums.length; i++) {
+    let sum = 0;
+    for (let j = i; j < nums.length; j++) {
+      sum += nums[j];
+      max = Math.max(max, sum);
+    }
+  }
+  return max;
 };
 // @lc code=end
 
+// @after-stub-for-debug-begin
+module.exports = maxSubArray;
+// @after-stub-for-debug-end
+
+// * 暴力双层循环, 解法正确但是会超时
+// var maxSubArray = function (nums) {
+//   if (!nums) return;
+//   if (nums.length === 1) {
+//     return nums[0];
+//   }
+//   let max = -Infinity;
+//   for (let i = 0; i < nums.length; i++) {
+//     let sum = 0;
+//     for (let j = i; j < nums.length; j++) {
+//       sum += nums[j];
+//       max = Math.max(max, sum);
+//     }
+//   }
+//   return max;
+// };
