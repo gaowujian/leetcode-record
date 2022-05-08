201c186639147607ea48dc08a868f319f68b76e5
tony <806003097@qq.com>
2022-05-08 19:20:42 +0800

    update

--- 257.二叉树的所有路径.js
+++ 257.二叉树的所有路径.js
@@ -18,31 +18,58 @@
  * @return {string[]}
  */
 
-// node是传入的节点，path是到该节点之前的路径，result是最终的结果
-function traversal(node, prevPath, result) {
-  if (!node) return;
-  const path = prevPath.length === 0 ? node.val.toString() : prevPath + "->" + node.val;
-  if (!node.left && !node.right) {
-    result.push(path);
-  } else {
-    traversal(node.left, path, result);
-    traversal(node.right, path, result);
-  }
-}
-
 var binaryTreePaths = function (root) {
   if (!root) return [];
   const result = [];
-  traversal(root, "", result);
+  const paths = [];
+  function traverse(node) {
+    if (!node) return;
+    // 前置位置，遍历到这个节点就放入path中
+    paths.push(node.val);
+    // 如果不是叶子节点，直接放入
+    if (!node.left && !node.right) {
+      result.push(paths.join("->"));
+    }
+    traverse(node.left);
+    traverse(node.right);
+    // 遍历完该节点后需要弹出
+    paths.pop();
+  }
+  traverse(root);
   return result;
 };
+
 // @lc code=end
 
 // @after-stub-for-debug-begin
 module.exports = binaryTreePaths;
 // @after-stub-for-debug-end
 
-// 1. 第一种 深度优先遍历的递归写法，无回溯
+// 1.第一种 遍历树的解法
+
+// var binaryTreePaths = function (root) {
+//   if (!root) return [];
+//   const result = [];
+//   // node是传入的节点，path是到该节点之前的路径，result是最终的结果
+//   const paths = [];
+//   function traverse(node) {
+//     if (!node) return;
+//     // 节点遍历前入栈
+//     paths.push(node.val);
+//     if (!node.left && !node.right) {
+//       result.push(paths.join("->"));
+//     } else {
+//       traverse(node.left);
+//       traverse(node.right);
+//     }
+//     // 节点遍历后入栈
+//     paths.pop();
+//   }
+//   traverse(root);
+//   return result;
+// };
+
+// 2. 分解子问题解法
 // var binaryTreePaths = function (root) {
 //   if (!root) {
 //     return [];
@@ -55,25 +82,4 @@ module.exports = binaryTreePaths;
 //           return `${root.val}->${item}`;
 //         });
 //   return converted;
-// };
-
-// 2. 第二种 递归算法
-// node是传入的节点，path是到该节点之前的路径，result是最终的结果
-// function traversal(node, prevPath, result) {
-//   if (node) {
-//     const path = prevPath.length === 0 ? node.val.toString() : prevPath + "->" + node.val;
-//     if (!node.left && !node.right) {
-//       result.push(path);
-//     } else {
-//       traversal(node.left, path, result);
-//       traversal(node.right, path, result);
-//     }
-//   }
-// }
-
-// var binaryTreePaths = function (root) {
-//   if (!root) return [];
-//   const result = [];
-//   traversal(root, "", result);
-//   return result;
 // };
