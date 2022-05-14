57e1b425356acf31e487797cdb1149e7f05f23ce
tony <806003097@qq.com>
2022-05-14 15:54:32 +0800

    零钱兑换问题

--- 509.斐波那契数.js
+++ 509.斐波那契数.js
@@ -9,20 +9,18 @@
  * @param {number} n
  * @return {number}
  */
+const memo = {};
 
 var fib = function (n) {
   if (n === 0 || n === 1) {
     return n;
   }
-  const dp = [];
-  dpA = 0;
-  dpB = 1;
-  for (let i = 2; i <= n; i++) {
-    dp[i] = dpA + dpB;
-    dpA = dpB;
-    dpB = dp[i];
+  if (memo[n]) {
+    return memo[n];
   }
-  return dp[n];
+  const result = fib(n - 1) + fib(n - 2);
+  memo[n] = result;
+  return result;
 };
 
 // @lc code=end
@@ -30,3 +28,18 @@ var fib = function (n) {
 // @after-stub-for-debug-begin
 module.exports = fib;
 // @after-stub-for-debug-end
+
+// var fib = function (n) {
+//   if (n === 0 || n === 1) {
+//     return n;
+//   }
+//   const dp = [];
+//   dpA = 0;
+//   dpB = 1;
+//   for (let i = 2; i <= n; i++) {
+//     dp[i] = dpA + dpB;
+//     dpA = dpB;
+//     dpB = dp[i];
+//   }
+//   return dp[n];
+// };
