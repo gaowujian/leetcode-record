74c467f9a6b5a0517e79d4d2c7855cc66b957aa0
tony <806003097@qq.com>
2022-05-16 00:14:31 +0800

    新学一个回溯

---
+++ 100.相同的树.js
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=100 lang=javascript
+ *
+ * [100] 相同的树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function (p, q) {
+  if (!p && !q) {
+    return true;
+  }
+  if (!p || !q) {
+    return false;
+  }
+  if (p.val !== q.val) {
+    return false;
+  }
+  const left = isSameTree(p.left, q.left);
+  const right = isSameTree(p.right, q.right);
+  return left && right;
+};
+// @lc code=end
+
+// @after-stub-for-debug-begin
+module.exports = isSameTree;
+// @after-stub-for-debug-end
